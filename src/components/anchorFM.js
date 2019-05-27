830f0babec54fcc796b0294804ebb70d1154a880
axecopfire <sbraun990@gmail.com>
2019-05-27 00:42:37 -0400

    added basic hover effect

--- src/components/anchorFM.js
+++ src/components/anchorFM.js
@@ -5,6 +5,9 @@ export default class AnchorFM extends React.Component {
     render () {
         return (
             <a href="https://anchor.fm/the-lunch-podcast">
+                <div className="hover-effect">
+                    <p>Listen to the Podcast</p>
+                </div>
                 <img src={logo} 
                 // className="App-logo" 
                 alt="logo" />
