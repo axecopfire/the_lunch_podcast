34f2e1908296c59cfa24d153ba193f2bc46d3908
axecopfire <sbraun990@gmail.com>
2019-06-03 21:59:03 -0400

    added a clipboard list

---
+++ src/components/clipboards.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Clipboard from './clipboard';
+
+export default class Clipboards extends React.Component {
+    render () {
+        return (
+            <div>
+                <Clipboard />
+                <Clipboard />
+                <Clipboard />
+                <Clipboard />
+                <Clipboard />
+                <Clipboard />
+                <Clipboard />
+            </div>
+        )
+    }
+}
