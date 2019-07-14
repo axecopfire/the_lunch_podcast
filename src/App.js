95296589e87da77fdcad1bfd4b19ef4195a2e01b
axecopfire <sbraun990@gmail.com>
2019-07-14 13:34:27 -0400

    routes work and styled navigation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import './styles/App.css';
-import TwitterFeed from './components/twitter_feed';
-import AnchorFM from './components/anchorFM';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Clipboards from "./components/clipboards";
 import Rss from './components/rss';
+import Home from './components/home';
 
 function App() {
 
   return (
     <div className="App">
+      <header className="App-header">
+        <a href="/" ><h1>The Lunch Podcast</h1></a>
+      </header>
       <Router>
         <Route path="/clipboard" component={Clipboards} />
       </Router>
-      <header className="App-header">
-        <h1>The Lunch Podcast</h1>
-      </header>
-      <Rss />
-      {/* <section className="social-media" >
-        <AnchorFM />
-      </section>
-      <section className="twitter-feed">
-        <TwitterFeed />
-      </section> */}
+      <Router>
+        <Route path="/episodes" component={Rss} />
+      </Router>
+      <Router>
+        <Route path="/" exact component={Home} />
+      </Router>
       
     </div>
   );
